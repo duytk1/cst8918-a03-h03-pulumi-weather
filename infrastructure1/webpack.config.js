6a9e052488ef213da7fff1bfbda0041aaf2c8127
D <duytk.job@gmail.com>
2025-01-29 19:56:57 -0500

    update screenshot location

---
+++ infrastructure1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust the entry point based on your project structure
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production', // Set to 'development' for non-minified output
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
